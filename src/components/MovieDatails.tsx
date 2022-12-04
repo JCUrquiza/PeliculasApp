f616a4bb0a885ff13511720a3ae741404d60fe22
Juan <gurquiza8@gmail.com>
2022-12-04 14:42:26 -0600

    completando detalles de movies

--- src/components/MovieDatails.tsx
+++ src/components/MovieDatails.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import currencyFormatter from 'currency-formatter';
 import { Cast } from '../interfaces/creditsInterfaces'
 import { MovieFull } from '../interfaces/movieInterfaces'
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -26,6 +27,23 @@ export const MovieDatails = ({ movieFull, cast }: Props) => {
                         - { movieFull.genres.map( g => g.name).join(', ') }
                     </Text>
                 </View>
+
+                {/* Historia */}
+                <Text style={{ fontSize: 23, marginTop: 10, fontWeight: 'bold' }}>
+                    Historia
+                </Text>
+                <Text style={{ fontSize: 16 }}>
+                    { movieFull.overview }
+                </Text>
+
+                {/* Presupuesto */}
+                <Text style={{ fontSize: 23, marginTop: 10, fontWeight: 'bold' }}>
+                    Presupuesto
+                </Text>
+                <Text style={{ fontSize: 18 }}>
+                    { currencyFormatter.format(movieFull.budget, { code: 'USD' }) }
+                </Text>
+
             </View>
         </>
     )
