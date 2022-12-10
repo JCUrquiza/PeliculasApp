04fbe4f456e722c86e436df330c425077459e7cd
Juan <gurquiza8@gmail.com>
2022-12-10 13:01:52 -0600

    Implementando la libreria lineragradient

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, ActivityIndicator, Dimensions, ScrollView } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
+import { GradientBackground } from '../components/GradientBackground';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
@@ -22,25 +23,30 @@ export const HomeScreen = () => {
     }
 
     return (
-        <ScrollView>        
-            <View style={{ marginTop: top + 20 }}>
-                {/* Carousel principal */}
-                <View style={{ height: 440 }}>
-                    <Carousel 
-                        data={ nowPlaying }
-                        renderItem={ ({item}: any) => <MoviePoster movie={ item } /> }
-                        sliderWidth={ windowWidth }
-                        itemWidth={ 300 }
-                        inactiveSlideOpacity={ 0.9 }
-                    />            
+        <GradientBackground>
+
+            <ScrollView>        
+
+                <View style={{ marginTop: top + 20 }}>
+                    {/* Carousel principal */}
+                    <View style={{ height: 440 }}>
+                        <Carousel 
+                            data={ nowPlaying }
+                            renderItem={ ({item}: any) => <MoviePoster movie={ item } /> }
+                            sliderWidth={ windowWidth }
+                            itemWidth={ 300 }
+                            inactiveSlideOpacity={ 0.9 }
+                        />            
+                    </View>
+
+                    <HorizontalSlider title="Populares" movies={ popular } />
+                    <HorizontalSlider title="Top Rated" movies={ topRated } />
+                    <HorizontalSlider title="Por venir" movies={ upcoming } />
                 </View>
 
-                <HorizontalSlider title="Populares" movies={ popular } />
-                <HorizontalSlider title="Top Rated" movies={ topRated } />
-                <HorizontalSlider title="Por venir" movies={ upcoming } />
+            </ScrollView>
 
-            </View>
-        </ScrollView>
+        </GradientBackground>
     )
 
 }
