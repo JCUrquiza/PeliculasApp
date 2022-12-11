dcb36771e3ccc757e2dbf2a6eac9d0f9749ec792
Juan <gurquiza8@gmail.com>
2022-12-10 21:32:59 -0600

    Terminando proyecto de apiMovies con fadein y out en background contextcolors

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, ActivityIndicator, Dimensions, ScrollView } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import ImageColors from 'react-native-image-colors'
 import Carousel from 'react-native-snap-carousel';
 import { GradientBackground } from '../components/GradientBackground';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
+import { getImageColors } from '../helpers/getColors';
+import { GradientContext } from '../context/GradientContext';
+import { useEffect } from 'react';
 
 const { width: windowWidth } = Dimensions.get('window');
 
@@ -13,6 +17,24 @@ export const HomeScreen = () => {
 
     const { isLoading, nowPlaying, popular, topRated, upcoming } = useMovies();
     const { top } = useSafeAreaInsets();
+    const { setMainColors } = useContext(GradientContext);
+
+    const getPosterColors = async (index: number) => {
+
+        const movie = nowPlaying[index];
+        const uri = `https://image.tmdb.org/t/p/w500${ movie.poster_path }`;
+
+        const [ primary = 'green', secondary = 'orange' ] = await getImageColors(uri);
+
+        setMainColors({ primary, secondary });
+        
+    }
+
+    useEffect(() => {
+        if(nowPlaying.length > 0) {
+            getPosterColors(0);
+        }
+    }, [ nowPlaying ])
 
     if(isLoading) {
         return (
@@ -36,6 +58,7 @@ export const HomeScreen = () => {
                             sliderWidth={ windowWidth }
                             itemWidth={ 300 }
                             inactiveSlideOpacity={ 0.9 }
+                            onSnapToItem={ index => getPosterColors(index) }
                         />            
                     </View>
 
